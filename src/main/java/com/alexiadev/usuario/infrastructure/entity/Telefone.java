3c9344a3ede0ef0dada26c3ab46282f1749df363
Alexia <alexiacastro2000@gmail.com>
2026-07-17 00:32:48 -0300

    Adicionadas classes de security

---
+++ src/main/java/com/alexiadev/usuario/infrastructure/entity/Telefone.java
@@ -0,0 +1,26 @@
+package com.alexiadev.usuario.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity //tabela de banco de dados
+@Table (name = "telefone")//nome da tabela
+
+public class Telefone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "numero", length = 10)
+    private String numero;
+    @Column(name = "ddd", length = 3)
+    private String ddd;
+}
+
