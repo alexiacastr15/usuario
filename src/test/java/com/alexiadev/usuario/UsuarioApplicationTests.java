c91468f1b699b016d4a6b5c40bd355985595e5b6
Alexia <alexiacastro2000@gmail.com>
2026-07-17 00:08:00 -0300

    commit inicial

---
+++ src/test/java/com/alexiadev/usuario/UsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexiadev.usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
