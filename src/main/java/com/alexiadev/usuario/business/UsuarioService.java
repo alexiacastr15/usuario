0433375eca9e8cbc1eaa34e11619fd839e062373
Alexia <alexiacastro2000@gmail.com>
2026-07-31 23:13:49 -0300

    inclusão de método put de usuário

--- src/main/java/com/alexiadev/usuario/business/UsuarioService.java
+++ src/main/java/com/alexiadev/usuario/business/UsuarioService.java
@@ -6,6 +6,7 @@ import com.alexiadev.usuario.infrastructure.entity.Usuario;
 import com.alexiadev.usuario.infrastructure.exceptions.ConflictException;
 import com.alexiadev.usuario.infrastructure.exceptions.ResourceNotFoundException;
 import com.alexiadev.usuario.infrastructure.repository.UsuarioRepository;
+import com.alexiadev.usuario.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,6 +19,8 @@ public class UsuarioService {
     private final UsuarioRepository usuarioRepository; //injeção de dependência da interface UsuarioRepository
     private final UsuarioConverter usuarioConverter;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
 
 
     public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO){ //recebeu objeto usuarioDTO
@@ -54,4 +57,21 @@ public class UsuarioService {
         usuarioRepository.deleteByEmail(email);
     }
 
+    public UsuarioDTO atualizaDadosUsuario(String token, UsuarioDTO dto){
+        //Buscar email atraves do token (tirar obrigatoriedade do email)
+        String email = jwtUtil.extrairEmailToken(token.substring(7));
+
+        //Criptografia de senha
+        dto.setSenha(dto.getSenha() != null ? passwordEncoder.encode(dto.getSenha()) : null);
+
+        //Buscar dados do usuario no banco de dados
+        Usuario usuarioEntity = usuarioRepository.findByEmail(email).orElseThrow(() ->
+                new ResourceNotFoundException("Email não localizado")); //obrigatorio por estar usando um opcional
+
+        //Mesclou dados que recebeu na requisição DTO com os dados do banco de dados
+        Usuario usuario = usuarioConverter.updateUsuario(dto, usuarioEntity);
+
+        //salvou dados do usuário convertido e depois pegou o retorno e converteu para UsuárioDTO
+        return usuarioConverter.paraUsuarioDTO(usuarioRepository.save(usuario));
+    }
 }
--- src/main/java/com/alexiadev/usuario/business/converter/UsuarioConverter.java
+++ src/main/java/com/alexiadev/usuario/business/converter/UsuarioConverter.java
@@ -98,4 +98,15 @@ public class UsuarioConverter {
                 .ddd(telefoneDTO.getDdd())
                 .build();
     }
+
+    public Usuario updateUsuario(UsuarioDTO usuarioDTO, Usuario entity){
+        return Usuario.builder()
+                .nome(usuarioDTO.getNome() != null ? usuarioDTO.getNome() : entity.getNome())
+                .id(entity.getId())
+                .senha(usuarioDTO.getSenha() != null ? usuarioDTO.getSenha() : entity.getSenha())
+                .email(usuarioDTO.getEmail() != null ? usuarioDTO.getEmail() : entity.getEmail())
+                .telefones(entity.getTelefones())
+                .enderecos(entity.getEnderecos())
+                .build();
+    }
 }
